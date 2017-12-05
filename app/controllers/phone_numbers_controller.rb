4bfd9cc98e6538425bbc826b5dd661a077f72aa8
samwel <samwuelkinuthia@gmail.com>
2017-12-05 18:19:51 +0300

    use javascript after creation

--- app/controllers/phone_numbers_controller.rb
+++ app/controllers/phone_numbers_controller.rb
@@ -2,9 +2,13 @@ class PhoneNumbersController < ApplicationController
   def new
     @phone_number = PhoneNumber.new
   end
+
   def create
     @phone_number = PhoneNumber.find_or_create_by(phone_number: params[:phone_number][:phone_number])
     @phone_number.generate_pin
     @phone_number.send_pin
+    respond_to do |format|
+      format.js
+    end
   end
 end
