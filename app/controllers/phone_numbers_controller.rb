e2455d820560c9faaa7c404e63a3f17e635ec82d
samwel <samwuelkinuthia@gmail.com>
2017-12-05 18:18:06 +0300

    create action. Find phone number

--- app/controllers/phone_numbers_controller.rb
+++ app/controllers/phone_numbers_controller.rb
@@ -2,4 +2,7 @@ class PhoneNumbersController < ApplicationController
   def new
     @phone_number = PhoneNumber.new
   end
+  def create
+    @phone_number = PhoneNumber.find_or_create_by(phone_number: params[:phone_number][:phone_number])
+  end
 end
