2c4cfbaa4de7e823f9e985f7efa36fb3e6b262ee
samwel <samwuelkinuthia@gmail.com>
2017-12-05 18:18:32 +0300

    call generate pin for phone number

--- app/controllers/phone_numbers_controller.rb
+++ app/controllers/phone_numbers_controller.rb
@@ -4,5 +4,6 @@ class PhoneNumbersController < ApplicationController
   end
   def create
     @phone_number = PhoneNumber.find_or_create_by(phone_number: params[:phone_number][:phone_number])
+    @phone_number.generate_pin
   end
 end
